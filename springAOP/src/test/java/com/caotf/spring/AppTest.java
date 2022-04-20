f7e294ed3141997fab0e1199e4904b6abca1e87d
caotengf <38907307+codeWorldRookie@users.noreply.github.com>
2022-04-20 10:45:14 +0800

    整合aspect
    spring 整个aspect基本操作

---
+++ springAOP/src/test/java/com/caotf/spring/AppTest.java
@@ -0,0 +1,24 @@
+package com.caotf.spring;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        assertNotNull(context);
+        Service service  = (Service) context.getBean("service");
+        service.buy();
+    }
+}
